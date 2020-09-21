ac0a7d9933f048f1f449a8e386906dfe38f1d1c2
zs <zs@bjpowernode.com>
2020-09-21 19:28:39 +0800

    Ribbon负载均衡策略自定义配置替换案例

--- cloud-consumer-order80/src/main/java/com/itheima/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/itheima/springcloud/controller/OrderController.java
@@ -1,15 +1,19 @@
 package com.itheima.springcloud.controller;
 
+import com.itheima.myrule.MyselfRule;
 import com.itheima.springcloud.entities.CommonResult;
 import com.itheima.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
 @RequestMapping("/consumer")
+@RibbonClient(name= "CLOUD-PAYMENT-SERVICE",configuration=MyselfRule.class)
 public class OrderController {
 //    public static final String PAYMENT_URL="http://localhost:8001";
     public static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE";
@@ -26,4 +30,15 @@ public class OrderController {
     public CommonResult getById(@PathVariable Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    @GetMapping("/payment/getForEntity/{id}")
+    public CommonResult getById2(@PathVariable Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }
+        return new CommonResult(444,"操作失败");
+    }
+
+
 }
