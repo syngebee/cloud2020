4d73a44179a8b4e4412eb943fc311ae44737c577
zs <zs@bjpowernode.com>
2020-10-15 19:15:37 +0800

    Sentinel 热点Key测试完成

--- cloudalibaba-sentinel-service8401/src/main/java/com/itheima/cloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/itheima/cloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.itheima.cloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -37,5 +40,16 @@ public class FlowLimitController {
         return "------testD";
     }
 
+    @GetMapping("/testHotKey")
+    @SentinelResource(value="testHotKey",blockHandler = "deal_testHotKey") //唯一即可
+    public String testHotKey(@RequestParam(value = "p1",required = false)String p1,
+                             @RequestParam(value = "p2",required = false)String p2){
+        return "------testHotKey";
+    }
+
+    @GetMapping("/deal_testHotKey")
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "------deal_testHotKey, o(╥﹏╥)o";
+    }
 
 }
