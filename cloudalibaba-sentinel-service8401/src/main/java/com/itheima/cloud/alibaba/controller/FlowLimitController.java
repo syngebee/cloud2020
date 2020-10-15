8c408930256bd33c201b7c46812128402f5f83f0
zs <zs@bjpowernode.com>
2020-10-15 16:25:18 +0800

    Sentinel 流控规则,降级规则测试完成.

--- cloudalibaba-sentinel-service8401/src/main/java/com/itheima/cloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/itheima/cloud/alibaba/controller/FlowLimitController.java
@@ -1,9 +1,13 @@
 package com.itheima.cloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA(){
@@ -14,4 +18,24 @@ public class FlowLimitController {
     public String testB(){
         return "-------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age=10/0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testD 异常数");
+        int age=10/0;
+        return "------testD";
+    }
+
+
 }
